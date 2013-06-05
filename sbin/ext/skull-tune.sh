98b8f116d7337b856e629f839e33a5132fcffb0f
Keff <funnykeff91@gmail.com>
2013-06-05 18:22:33 +0800

    This greatly reduce ramdisk size and fix STUPID bootloop problem also! My bad.
    Signed-off-by: Keff <funnykeff91@gmail.com>

--- sbin/ext/skull-tune.sh
+++ sbin/ext/skull-tune.sh
@@ -3,15 +3,8 @@
 BB="/sbin/busybox"
 
 mount -o remount,rw /system
-$BB mount -t rootfs -o remount,rw rootfs
 
-# Run init scripts
-if [ -d /system/etc/init.d ]; then
-	chmod 755 /system/etc/init.d/*
-	$BB run-parts /system/etc/init.d
-fi;
-
-# Frandom hack
+# Hacked random and urandom for frandom
 mv /dev/random /dev/random.ori
 mv /dev/urandom /dev/urandom.ori
 ln /dev/frandom /dev/random
@@ -19,5 +12,4 @@ chmod 666 /dev/random
 ln /dev/erandom /dev/urandom
 chmod 666 /dev/urandom
 
-$BB mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
